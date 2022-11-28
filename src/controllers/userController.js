5b42ddbbbdcfc6214eabd2cc7b9e898335bfc548
Eder-R <eder31250@gmail.com>
2022-11-28 16:07:28 -0300

    Sequelize pré-initialized

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,5 @@
+const connection = require('../database/connection')
+
 const responseModel = {
   success: false,
   data: [],
@@ -5,9 +7,30 @@ const responseModel = {
 }
 module.exports = {
   
-  async list(req, res) {
+  async create(req, res) {
     const response = {...responseModel}
 
+    const { username, password } = req.body
+
+    const [, affectRows ] = await connection.query(`
+      INSERT INTO users VALUES (DEFAULT, '${username}', '${password}', NOW(), NOW())
+    `)
+
+    response.success = affectRows > 0
+
+    return res.json(response)
+  },
+
+  async login(req, res) {
+    const response = {...responseModel}
+
+    const { username, password } = req.body
+
+    const [, data] = await connection.query(`
+      SELECT * FROM users WHERE username='${username}' AND password='${password}'
+    `)
+
+    console.log(data)
 
     return res.json(response)
   }
